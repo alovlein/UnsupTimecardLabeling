8028803d26da40290d667ce827a6b5e5d47789a1
Alec Lovlein <aleclovlein@Alecs-MacBook-Pro-2.local>
2021-03-13 08:49:44 -0600

    updates to add cluster labeling

--- Solution.py
+++ Solution.py
@@ -7,7 +7,7 @@ from sklearn.cluster import KMeans
 from transformers import BertTokenizer, BertModel, pipeline
 import torch
 from progress.bar import IncrementalBar
-from joblib import dump
+from joblib import dump, load
 
 
 def relativize_dates_(df):
@@ -35,7 +35,13 @@ def relativize_dates_(df):
     return True
 
 
-data = pd.read_csv('data/ailbiz_challenge_data.csv')
+def analyze_clusters(data):
+    df = pd.DataFrame(data, columns=['Cluster', 'Label', 'Score'])
+    redux = df.groupby(by=['Cluster', 'Label'], as_index=False).sum().sort_values('Score', ascending=False)
+    print(redux)
+
+
+data = pd.read_csv('data/ailbiz_challenge_data.csv')[:300]
 codes = pd.read_csv('data/ailbiz_challenge_codeset.csv')
 
 relativize_dates_(data)
@@ -52,21 +58,33 @@ for ind, line in enumerate(data['Narrative']):
             )
         ).unsqueeze(0))[0]
     raw_features.append(np.append(np.average(embedded.detach().numpy(), axis=1).flatten(),
-                                  data['Timeline_Completion'][ind]))
+                                  [data['Timeline_Completion'][ind],
+                                  data['Effort'][ind]]))
     bar.next()
 print()
-
 scaler = StandardScaler()
 features = scaler.fit_transform(raw_features)
 model = KMeans(n_clusters=len(codes))
 labels = model.fit_predict(features)
 
 classifier = pipeline('zero-shot-classification', model='facebook/bart-large-mnli')
-cluster_labels = []
-bar = IncrementalBar('Classifying', max=data.shape[0], suffix='%(percent)d%%')
-for ind, sequence in enumerate(data['Narrative']):
-    suggestion = classifier(sequence, codes['Description'])
-    cluster_labels.append([labels[ind], suggestion['labels'][0], suggestion['scores'][0]])
-    bar.next()
-print()
-dump(cluster_labels)
+
+classifications = []
+for i in range(len(codes)):
+    matchind = np.where([labels == i])[1]
+    occupation = len(matchind)
+    size = np.minimum(100, occupation)
+    cutind = np.random.choice(a=occupation, size=size, replace=False)
+    testind = matchind[cutind]
+    test_set = data['Narrative'][testind]
+    bar = IncrementalBar(f'Classifying group {(i + 1):02d}/{len(codes)}', max=size, suffix='%(percent)d%%')
+    for sequence in test_set:
+        suggestion = classifier(sequence, codes['Description'])
+        classifications.append([i, suggestion['labels'][0], suggestion['scores'][0]])
+        bar.next()
+    print()
+
+dump(classifications, 'cluster_labels.joblib')
+classifications = load('cluster_labels.joblib')
+
+analyze_clusters(classifications)
